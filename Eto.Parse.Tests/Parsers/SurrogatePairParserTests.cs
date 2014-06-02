b952b5a36d8f1b1ae76d81b9d8e92c9db4074187
tpluscode <tomasz@t-code.pl>
2014-06-02 18:11:31 +0200

    Initial test for matching any surrogate pair character

---
+++ Eto.Parse.Tests/Parsers/SurrogatePairParserTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Eto.Parse.Parsers;
+using NUnit.Framework;
+
+namespace Eto.Parse.Tests.Parsers
+{
+    [TestFixture]
+    public class SurrogatePairParserTests
+    {
+        [Test]
+        public void TestAnySurrogatePair()
+        {
+            var chars = string.Format("{0},{1},{2}",
+                char.ConvertFromUtf32(0x10000),
+                char.ConvertFromUtf32(0x87FFF),
+                char.ConvertFromUtf32(0x10FFFF));
+
+            var grammar = new Grammar();
+            var parser = new SurrogatePairParser();
+            grammar.Inner = (+parser.Named("char")).SeparatedBy(",");
+
+            var match = grammar.Match(chars);
+
+            Assert.IsTrue(match.Success, match.ErrorMessage);
+            CollectionAssert.AreEquivalent(new []{0x10000, 0x87FFF, 0x10FFFF}, match.Find("char").Select(parser.GetValue));
+        }
+    }
+}
